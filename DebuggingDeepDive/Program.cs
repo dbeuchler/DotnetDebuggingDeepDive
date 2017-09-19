24cdb2866fddca15d05e9d90d72f11c434815dd2
dbeuchler <dennis@beuchler.net>
2017-09-19 20:56:08 +0200

    added three debugging options

--- DebuggingDeepDive/Program.cs
+++ DebuggingDeepDive/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DebuggingDeepDive
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const int StringElements = 1000;
+        private const int ByteElements = 104857600; // ~ 100 MB
+        private const int ClassElements = 1000;
+
+        private static void Main(string[] args)
         {
+            var program = new Program();
+            program.Start();
+
+            Console.ReadKey();
         }
+
+        private void Start()
+        {
+            var rnd = new Random();
+            
+            Console.Write("1/3: Creating a list of string items ...");
+            var stringList = new List<string>(StringElements);
+            for (var i = 0; i < StringElements; i++)
+                stringList.Add($"Element: {i}");
+            Console.WriteLine(" {0:N} done!", stringList.Count);
+
+            Console.ReadKey();
+
+            Console.Write("2/3: Creating an array of random bytes ...");
+            var byteArray = new byte[ByteElements];
+            rnd.NextBytes(byteArray);
+            Console.WriteLine(" {0:N} done!", byteArray.Length);
+
+            Console.ReadKey();
+
+            Console.Write("3/3: Creating a list of object items ...");
+            var objList = new List<SomeClass>(StringElements);
+            for (var i = 0; i < ClassElements; i++)
+                objList.Add(new SomeClass {OneString = $"Element: {i}"});
+            Console.WriteLine(" {0:N} done!", objList.Count);
+        }
+    }
+
+    public class SomeClass
+    {
+        public string OneString { get; set; }
     }
 }
