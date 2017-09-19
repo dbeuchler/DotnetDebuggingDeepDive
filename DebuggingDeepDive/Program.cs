a92ec38f533d6bf69ad9c6b00d4641d8bb020cd6
dbeuchler <dennis@beuchler.net>
2017-09-19 21:19:11 +0200

    switched order of executions

--- DebuggingDeepDive/Program.cs
+++ DebuggingDeepDive/Program.cs
@@ -13,8 +13,6 @@ namespace DebuggingDeepDive
         {
             var program = new Program();
             program.Start();
-
-            Console.ReadKey();
         }
 
         private void Start()
@@ -24,23 +22,25 @@ namespace DebuggingDeepDive
             Console.Write("1/3: Creating a list of string items ...");
             var stringList = new List<string>(StringElements);
             for (var i = 0; i < StringElements; i++)
-                stringList.Add($"Element: {i}");
+                stringList.Add($"Element: {i:D4}");
             Console.WriteLine(" {0:N} done!", stringList.Count);
 
             Console.ReadKey();
 
-            Console.Write("2/3: Creating an array of random bytes ...");
+            Console.Write("2/3: Creating a list of object items ...");
+            var objList = new List<SomeClass>(StringElements);
+            for (var i = 0; i < ClassElements; i++)
+                objList.Add(new SomeClass { OneString = $"Element: {i:D4}" });
+            Console.WriteLine(" {0:N} done!", objList.Count);
+
+            Console.ReadKey();
+
+            Console.Write("3/3: Creating an array of random bytes ...");
             var byteArray = new byte[ByteElements];
             rnd.NextBytes(byteArray);
             Console.WriteLine(" {0:N} done!", byteArray.Length);
 
             Console.ReadKey();
-
-            Console.Write("3/3: Creating a list of object items ...");
-            var objList = new List<SomeClass>(StringElements);
-            for (var i = 0; i < ClassElements; i++)
-                objList.Add(new SomeClass {OneString = $"Element: {i}"});
-            Console.WriteLine(" {0:N} done!", objList.Count);
         }
     }
 
